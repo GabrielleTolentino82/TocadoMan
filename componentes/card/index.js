91be49e9f49741666e114f353de91be63f2b2d81
Gabi <gabi.tolentino23@gmail.com>
2024-05-06 14:12:36 -0300

    Quarta versão

--- componentes/card/index.js
+++ componentes/card/index.js
@@ -4,12 +4,12 @@ import estilo from './style';
 export default function Card({id,preco,titulo,imagem,descricao }){
     return(
         <View style={estilo.container}>
-            <Text style = {{fontSize: 20, textAlign: 'right', color: 'white'}}>{id}.</Text>
+            <Text style = {{fontSize: 25, textAlign: 'right', color: 'white', fontWeight: 'bold'}}>{id}.</Text>
         <View style={estilo.card}>
             <View style={{ marginLeft:15}}>
                   <Text style={estilo.titulo}>{titulo}</Text>
                 <Text style={estilo.texto}>{descricao}</Text>
-                <Text style={{fontSize: 35, color: 'white'}}>R$ {preco}</Text>
+                <Text style={{fontSize: 35, color: 'white', fontWeight: 'bold'}}>R$ {preco}</Text>
             </View>
             <Image style={estilo.imagem} source={{uri: imagem}}/>
 
